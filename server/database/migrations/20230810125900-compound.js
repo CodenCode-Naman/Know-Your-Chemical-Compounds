38cdada62699796e3c35c3dbf2d0e122bcaf0617
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 18:41:41 +0530

    chore: add seeders and migrations to db

---
+++ server/database/migrations/20230810125900-compound.js
@@ -0,0 +1,36 @@
+/* eslint-disable linebreak-style */
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Compounds', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      compoundName: {
+        type: Sequelize.STRING
+      },
+      compoundImage: {
+        type: Sequelize.STRING
+      },
+      compoundDescription: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Compounds');
+  }
+};
---
+++ server/database/seeders/20230810130354-compounds.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const filePath = path.join(__dirname, '..', 'compounds.json');
+    const { compounds } = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+    const compoundData = compounds.map(compound => ({
+      compoundName: compound.compoundName,
+      compoundImage: compound.compoundImage,
+      compoundDescription: compound.compoundDescription,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }));
+
+    await queryInterface.bulkInsert('Compounds', compoundData, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Compounds', null, {});
+  }
+};
