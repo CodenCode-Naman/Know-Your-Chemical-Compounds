deb822b66f275c4b6c360bd39269e3e9a92456ee
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 19:56:38 +0530

    feat: add card-item component to the cards

---
+++ client/src/app/components/card-item/card-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { Compound } from 'src/app/types/compound';
+
+@Component({
+  selector: 'app-card-item',
+  templateUrl: './card-item.component.html',
+  styleUrls: ['./card-item.component.css']
+})
+export class CardItemComponent {
+  @Input() compound: Compound;
+  @Output() onDeleteCompound: EventEmitter<Compound> = new EventEmitter();
+  @Output() onEditCompound: EventEmitter<Compound> = new EventEmitter();
+
+  faTrashCan = faTrashCan;
+  faPenToSquare = faPenToSquare;
+
+  onDelete(compound: Compound) {
+    this.onDeleteCompound.emit(compound);
+  }
+}
