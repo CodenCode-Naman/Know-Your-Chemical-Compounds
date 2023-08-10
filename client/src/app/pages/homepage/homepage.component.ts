06d398c39b0648d6196f871acee61e7305a48269
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 20:30:08 +0530

    feat: add the components to the homepage

--- client/src/app/pages/homepage/homepage.component.ts
+++ client/src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Compound } from 'src/app/types/compound';
+import { CompoundService } from '../../services/compound.service';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  compounds!: Compound[];
+  page!: number;
+  totalCompounds!: number;
+  totalPages!: number;
 
+  constructor(private compoundService: CompoundService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if ((Object.keys(params).length === 1 && !params['pg']) || Object.keys(params).length > 1) {
+        this.router.navigate(['/404'])
+        return;
+      }
+      this.page = Number(params['pg']) || 1;
+      this.compoundService.getCompounds(this.page).subscribe(response => {
+        this.compounds = response.rows;
+        this.totalCompounds = response.count;
+        this.totalPages = Math.ceil(this.totalCompounds / 3);
+      }, error => this.router.navigate(['/404']));
+    });
+  }
+
+  deleteCompound(compound: Compound) {
+    this.compoundService.deleteCompound(compound).subscribe(() => {
+      this.compounds = this.compounds.filter(t => t.id !== compound.id);
+      if (this.compounds.length === 0) {
+        this.router.navigate(['/compounds'], { queryParams: { pg: --this.page } });
+      }
+    });
+  }
 }
