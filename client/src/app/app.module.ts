a126cffd03965c2cd84f836e0fc5519c85ae588d
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 20:16:19 +0530

    feat: add services and card-detail pages to the client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardItemComponent } from './components/card-item/card-item.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardDetailsComponent } from './pages/card-details/card-details.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CompoundFormComponent } from './pages/compound-form/compound-form.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 @NgModule({
@@ -11,6 +15,10 @@ import { HeaderComponent } from './components/header/header.component';
     AppComponent,
     CardItemComponent,
     HeaderComponent,
+    CardDetailsComponent,
+    HomepageComponent,
+    CompoundFormComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule
---
+++ client/src/app/pages/compound-form/compound-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compound-form',
+  templateUrl: './compound-form.component.html',
+  styleUrls: ['./compound-form.component.css']
+})
+export class CompoundFormComponent {
+
+}
