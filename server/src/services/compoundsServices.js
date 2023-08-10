ae4ca5d9dd7091edbe2834185561cde265059081
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 18:57:15 +0530

    feat: implemented Update, insert, delete, upsert services for the compound

--- server/src/services/compoundsServices.js
+++ server/src/services/compoundsServices.js
@@ -23,7 +23,47 @@ const getCompoundById = async (id) => {
 
 };
 
+const createCompound = async (compound) => {
+  try {
+    return await db.Compound.create(compound);
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+};
+
+const updateCompound = async (id, compound) => {
+  try {
+    return await db.Compound.update(compound, { where: { id }, returning: true, plain: true });
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+};
+
+const deleteCompound = async (id) => {
+  try {
+    return await db.Compound.destroy({ where: { id } });
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+};
+
+const upsertCompounds = async (compounds) => {
+  try {
+    return await db.Compound.bulkCreate(compounds, { updateOnDuplicate: ['id'] });
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+};
+
 module.exports = {
   getAllCompounds,
   getCompoundById,
+  createCompound,
+  updateCompound,
+  deleteCompound,
+  upsertCompounds
 };
