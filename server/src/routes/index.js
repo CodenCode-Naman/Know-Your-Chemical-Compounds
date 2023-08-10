4851e9b1facc375646c4bc891a8f586806f985be
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 19:12:11 +0530

    chore: add requested routes to the server

---
+++ server/src/routes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const compoundsController = require('../controllers/compoundsController');
+const { validateCompound, validateQuery, validateId } = require('../middlewares/middlewares');
+
+router.route('/compounds')
+  .get(validateQuery, compoundsController.getAllCompounds)
+  .post(validateCompound, compoundsController.addCompound)
+  .put(compoundsController.upsertCompounds);
+
+router.route('/compounds/:id')
+  .get(validateId, compoundsController.getCompoundById)
+  .put(validateId, validateCompound, compoundsController.updateCompoundById)
+  .delete(validateId, compoundsController.deleteCompoundById);
+
+module.exports = router;
