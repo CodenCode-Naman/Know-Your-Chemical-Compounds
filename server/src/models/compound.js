6c50d71505965c04e340fc399da1f6ee08b92741
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-10 19:01:13 +0530

    chore: add models for the server

---
+++ server/src/models/compound.js
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable linebreak-style */
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Compound extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Compound.init({
+    compoundName: DataTypes.STRING,
+    compoundImage: DataTypes.STRING,
+    compoundDescription: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Compound',
+  });
+  return Compound;
+};
