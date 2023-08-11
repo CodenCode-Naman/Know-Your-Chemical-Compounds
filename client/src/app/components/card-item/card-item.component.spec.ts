882eacc46299babc7abe59abe78b9f483c5491d0
Naman Bhatia <nbhatia_be19@thapar.edu>
2023-08-11 13:35:54 +0530

    chore: add unit test for components in client

--- client/src/app/components/card-item/card-item.component.spec.ts
+++ client/src/app/components/card-item/card-item.component.spec.ts
@@ -1,21 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Compound } from 'src/app/types/compound';
 import { CardItemComponent } from './card-item.component';
 
 describe('CardItemComponent', () => {
   let component: CardItemComponent;
   let fixture: ComponentFixture<CardItemComponent>;
+  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CardItemComponent]
+      declarations: [CardItemComponent],
+      imports: [RouterTestingModule, FontAwesomeModule],
     });
+
     fixture = TestBed.createComponent(CardItemComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    router = TestBed.inject(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display compound name', () => {
+    const testCompound: Compound = {
+      id: 1,
+      compoundName: 'Test Compound',
+      compoundImage: 'test-image.jpg',
+      compoundDescription: ''
+    };
+
+    component.compound = testCompound;
+    fixture.detectChanges();
+
+    const cardTitle = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(cardTitle.textContent).toContain(testCompound.compoundName);
+  });
+
+  it('should emit onDeleteCompound event when delete icon is clicked', () => {
+    const testCompound: Compound = {
+      id: 1,
+      compoundName: 'Test Compound',
+      compoundImage: 'test-image.jpg',
+      compoundDescription: ''
+    };
+
+    component.compound = testCompound;
+    fixture.detectChanges();
+
+    let emittedCompound: Compound | undefined;
+    component.onDeleteCompound.subscribe((compound) => (emittedCompound = compound));
+
+    const deleteIcon = fixture.debugElement.query(By.css('.icon')).nativeElement;
+    deleteIcon.click();
+
+    expect(emittedCompound).toBe(testCompound);
+  });
+
+  it('should display compound image', () => {
+    const testCompound: Compound = {
+      id: 1,
+      compoundName: 'Test Compound',
+      compoundImage: 'test-image.jpg',
+      compoundDescription: ''
+    };
+
+    component.compound = testCompound;
+    fixture.detectChanges();
+
+    const compoundImage = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(compoundImage.src).toContain(testCompound.compoundImage);
   });
 });
--- client/src/app/components/header/header.component.spec.ts
+++ client/src/app/components/header/header.component.spec.ts
@@ -1,21 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let router: Router;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
-    });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the correct title', () => {
+    const compiled = fixture.nativeElement;
+    const titleElement = compiled.querySelector('h1');
+    expect(titleElement.textContent).toContain('Know Your Chemical Compounds');
+  });  
 });
